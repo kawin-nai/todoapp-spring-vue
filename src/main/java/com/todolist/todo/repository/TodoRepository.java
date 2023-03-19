313bd70064f8b52eccce0831d5f0d89656ec27e1
qazseee <qazseee@gmail.com>
2023-03-20 01:33:02 +0800

    Fixed query param. Start the page from 1 instead of 0.

--- src/main/java/com/todolist/todo/repository/TodoRepository.java
+++ src/main/java/com/todolist/todo/repository/TodoRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
-    @Query("select count(t) from Todo t where t.id = ?1")
-    long countById(Long id);
     @Transactional
     @Modifying
     @Query("update Todo t set t.id = ?1, t.text = ?2, t.checked = ?3 where t.id = ?4")
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,13 @@
 INSERT INTO todos (text, checked) VALUES
 ('Finish Final year Project', false),
 ('Finish this homework', false),
-('Revise for the exam', false);
+('Revise for the exam', false),
+('4', false),
+('5', false),
+('6', false),
+('7', false),
+('8', false),
+('9', false),
+('10', false),
+('11', false),
+('12', false);
