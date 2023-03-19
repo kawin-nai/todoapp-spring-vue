ee9a1508619b9c990e530cc9b7b63d11ed57d932
qazseee <qazseee@gmail.com>
2023-03-20 00:37:30 +0800

    Added Vue front-end

--- src/main/java/com/todolist/todo/repository/TodoRepository.java
+++ src/main/java/com/todolist/todo/repository/TodoRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     @Transactional
     @Modifying
